ce4740c3bfd9ab28dc4b62d54f7aab5494f52bb7
luke <812901530@qq.com>
2018-12-11 20:21:04 +0100

    add codes

---
+++ src/run_mf.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Useful starting lines
+import numpy as np
+# import scipy
+# import scipy.io
+# import scipy.sparse as sp
+
+dat_dir = '../data/'
+sub_dir = '../submit/'
+
+from pre_post_process import *
+
+_, ratings = load_data(dat_dir + "data_train.csv")
+sample_ids, _ = load_data(dat_dir + "sample_submission.csv")
+print(np.shape(ratings))
+
+valid_ratings, train, test = split_data(ratings, p_test=0.2)
+
+from SGD_helpers import *
+
+best_gamma = 0.025
+best_lambda_u = 0.1
+best_lambda_i = 0.01
+K = 20
+num_epochs = 50
+
+user_init, item_init = init_MF(ratings, K)
+item_feats_SGD, user_feats_SGD, rmse = matrix_factorization_SGD(ratings, test, best_gamma, K, best_lambda_u, best_lambda_i, num_epochs,
+                                                                    user_init, item_init, include_test = True)
+
+tempt_dir = '../tempt/'
+np.save(tempt_dir + 'item_feats_SGD.npy', item_feats_SGD)
+np.save(tempt_dir + 'user_feats_SGD.npy', user_feats_SGD)
+
+user_feats_SGD = np.load(tempt_dir + 'user_feats_SGD.npy')
+item_feats_SGD = np.load(tempt_dir + 'item_feats_SGD.npy')
+
+predictions = np.dot(item_feats_SGD.T, user_feats_SGD)
+print("Shape of predictions (user x items): {}".format(np.shape(predictions)))
+
+### Checking if results appear the same in the final excel file
+first_user, first_item = sample_ids[0][0],sample_ids[0][1]
+print(first_item, first_user)
+print(predictions[first_user - 1, first_item - 1])
+
+predictions[ np.where( predictions > 5.0 ) ] = 5.0
+predictions[ np.where(predictions < 1.0)] = 1.0
+print("Minimum prediction: {}, Maximum prediction: {}".format(np.min(predictions), np.max(predictions)))
+
+print("Shape of predictions (user x items): {}".format(np.shape(predictions)))
+wanted_preds = getWantedPredictions(predictions.T, sample_ids)
+create_csv_submission(sample_ids, np.round(wanted_preds), sub_dir + "bias_FULL_best_param_50epochs.csv")
